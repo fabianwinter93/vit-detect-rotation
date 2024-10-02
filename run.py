c09d487baea7e0448b862cd626e18bdb287f1bb8
fabianwinter93 <fabianwinter93@gmail.com>
2024-10-02 11:03:46 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -17,6 +18,8 @@ from tqdm import tqdm
 
 import torch
 torch.set_grad_enabled(False)
+
+print(print(torch.cuda.is_available()))
 
 import timm
 
@@ -83,24 +86,24 @@ if __name__ == "__main__":
     parser.add_argument("--quadro", action='store_true', help="If exactly YES, compare logit-confidence score for each rotation and pick the best, else use logits for prediction")
     parser.add_argument("--dry", action='store_true', help="If exactly YES, do nothing")
     
-    parser.add_argument("--no-gpu", action='store_true')
+    parser.add_argument("--cpu", action='store_true')
 
     parser.add_argument("--compile", action='store_true')
     parser.add_argument("--trace", action='store_true')
     parser.add_argument("--script", action='store_true')
 
-    parser.add_argument("--bf16", action='store_true')
-    parser.add_argument("--f16", action='store_true')
     parser.add_argument("--f32", action='store_true')
     
     parser.add_argument("--verbosity", default="2", help="verbosity level")
     
     args = parser.parse_args()
 
-    if not args.no_gpu:
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    else:
+    if not args.cpu:
         device = torch.device("cpu")
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print(f"Using {device}")
     
     RECURSIVE = args.recursive
     QUADRO = args.quadro
@@ -110,115 +113,102 @@ if __name__ == "__main__":
     assert VERBOSE in ["0", "1", "2"], "Only levels 0, 1, 2 are valid."
 
     
-    
-    
-    if args.bf16:
+    if args.f32:
+        dtype = torch.float32
+    else:
         dtype = torch.bfloat16
         model.bfloat16()
         
-    elif args.f16:
-        dtype = torch.float16
-        model.float16()
-        
-    else:
-        dtype = torch.float32
-        #model.float32()
-
-    
     model.to(device)
 
-    if args.compile:
-        model = torch.compile(model, backend="cudagraphs", fullgraph=True)
 
-    if args.script:
-        if QUADRO:
-            trace_inp = torch.rand((4, 3, 224, 224))
-        else:
-            trace_inp = torch.rand((1, 3, 224, 224))
-        model = torch.jit.script(model, trace_inp.to(dtype).to(device))
-    elif args.trace:
-        if QUADRO:
-            trace_inp = torch.rand((4, 3, 224, 224))
-        else:
-            trace_inp = torch.rand((1, 3, 224, 224))
-        model = torch.jit.trace(model, trace_inp.to(dtype).to(device))
-
-
-
-    #with torch.no_grad():
-        #pass
-        #model = torch.jit.script(model, trace_inp.bfloat16().to(device))
-        #model = torch.jit.optimize_for_inference(model)
-        #model = torch.compile(model, backend="cudagraphs", fullgraph=True)
+    with torch.inference_mode():
         
-    src_dir = os.path.abspath(args.source_dir)
-
-    source_files = find_files(src_dir, RECURSIVE)
-    #source_files = validate_src_files(source_files)
-    n_src_files = len(source_files)
-
-    assert n_src_files > 0, f"No valid files (jpg, png) found in {src_dir}"
+        if args.compile:
+            model = torch.compile(model, backend="cudagraphs", fullgraph=True)
     
-    print(f"Found {n_src_files} valid files in {src_dir}.")
-
-
-    predictions = {}
-
+        if args.script:
+            if QUADRO:
+                trace_inp = torch.rand((4, 3, 224, 224))
+            else:
+                trace_inp = torch.rand((1, 3, 224, 224))
+            model = torch.jit.script(model, trace_inp.to(dtype).to(device))
+        elif args.trace:
+            if QUADRO:
+                trace_inp = torch.rand((4, 3, 224, 224))
+            else:
+                trace_inp = torch.rand((1, 3, 224, 224))
+            model = torch.jit.trace(model, trace_inp.to(dtype).to(device))
     
-    num_images_to_reorient = 0
-
-
-    _range = range(n_src_files)
-    if VERBOSE in ["2"]:
-        _range = tqdm(_range)
+    
+    
+        #with torch.no_grad():
+            #pass
+            #model = torch.jit.script(model, trace_inp.bfloat16().to(device))
+            #model = torch.jit.optimize_for_inference(model)
+            #model = torch.compile(model, backend="cudagraphs", fullgraph=True)
+            
+        src_dir = os.path.abspath(args.source_dir)
+        src_dir = os.path.normpath(src_dir)
+    
+        source_files = find_files(src_dir, RECURSIVE)
+        #source_files = validate_src_files(source_files)
+        n_src_files = len(source_files)
+    
+        assert n_src_files > 0, f"No valid files (jpg, png) found in {src_dir}"
         
-    for i in _range:
-
-        fname = source_files[i]
-        fpath = os.path.join(src_dir, fname)
-
+        print(f"Found {n_src_files} valid files in {src_dir}.")
+    
+    
+        predictions = {}
+    
         
-        img = Image.open(fpath)
-        batch = prepare_batch(img, True).to(dtype)
+        num_images_to_reorient = 0
+    
+    
+        _range = range(n_src_files)
+        if VERBOSE in ["2"]:
+            _range = tqdm(_range)
+            
+        for i in _range:
+    
+            fname = source_files[i]
+            fpath = os.path.join(src_dir, fname)
+    
+            
+            img = Image.open(fpath)
+            batch = prepare_batch(img, True).to(dtype)
+            
+            pred = predict(batch)
+    
+            
+            #logits = model(inference_transforms(img)[None, ...].to(device))
+            #probs = torch.nn.functional.softmax(logits, -1)
+            #pred = torch.argmax(probs, -1).item()
+    
+            predictions[fpath] = pred
+    
+            if pred == 0:
+                if VERBOSE in ["0"]:
+                    print(fname)
+            else:
+                if VERBOSE in ["0", "1"]:
+                    print(f"{fname} - turn {90*pred}°")
+                    
+                num_images_to_reorient += 1
+    
+                targetpath = fpath
+                #targetpath = targetpath.replace(".jpg", "_COPY.jpg")
+                #targetpath = targetpath.replace(".png", "_COPY.png")
+    
+                rotated_img = rotate_image_lossless_transpose(img, 90*pred)
+                if DRY is False:
+                    rotated_img.save(targetpath)
+    
         
-        pred = predict(batch)
-
-        
-        #logits = model(inference_transforms(img)[None, ...].to(device))
-        #probs = torch.nn.functional.softmax(logits, -1)
-        #pred = torch.argmax(probs, -1).item()
-
-        predictions[fpath] = pred
-
-        if pred == 0:
-            if VERBOSE in ["0"]:
-                print(fname)
-        else:
-            if VERBOSE in ["0", "1"]:
-                print(f"{fname} - turn {90*pred}°")
-                
-            num_images_to_reorient += 1
-
-            targetpath = fpath
-            #targetpath = targetpath.replace(".jpg", "_COPY.jpg")
-            #targetpath = targetpath.replace(".png", "_COPY.png")
-
-            rotated_img = rotate_image_lossless_transpose(img, 90*pred)
-            if DRY is False:
-                rotated_img.save(targetpath)
-
+    
+        print(f"Found {num_images_to_reorient} images with incorrect orientation") 
+        if num_images_to_reorient == 0: exit()
     
 
-    print(f"Found {num_images_to_reorient} images with incorrect orientation") 
-    if num_images_to_reorient == 0: exit()
-
-
         
-
-
-
-
-
-
-
-
